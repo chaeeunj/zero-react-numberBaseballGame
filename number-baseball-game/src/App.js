59e2c8b96ed8b4f382a5acb026e5aa3e96e4a2e0
chaeeunj <official2930@naver.com>
2023-07-05 20:06:23 +0900

    숫자 야구 게임 기본 파일 구성

---
+++ number-baseball-game/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>숫자 야구 게임</h1>
+      <header className="header">----</header>
+      <section>
+        <input type="" />
+        <button>맞춰보기</button>
+      </section>
+      <h2>기록</h2>
+      <ol>
+        <li>1234 (strike: 0, ball: 2)</li>
+        <li>4567 (strike: 1, ball: 1)</li>
+        <li>7869 (strike: 1, ball: 1)</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
